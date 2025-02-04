8602da3b05a9ded1aca38e347241fd937671d6a8
anshugithub1 <anshusaurabh1ay@gmail.com>
2025-02-05 00:21:00 +0530

    committed Q34 Leetcode 3105.Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ Q34.cpp
@@ -0,0 +1,71 @@
+/*
+Leetcode-->3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+Problem Statement : 
+You are given an array of integers nums. Return the length of the longest 
+subarray of nums which is either strictly increasing or strictly decreasing.
+
+Example 1:
+Input: nums = [1,4,3,3,2]
+Output: 2
+Explanation:
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+Hence, we return 2.
+
+Example 2:
+Input: nums = [3,3,3,3]
+Output: 1
+Explanation:
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+Hence, we return 1.
+
+Example 3:
+Input: nums = [3,2,1]
+Output: 3
+Explanation:
+The strictly increasing subarrays of nums are [3], [2], and [1].
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+Hence, we return 3.
+
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+
+*/
+
+
+// Solution
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//------------------------------------->> Approach:1 (Kinda!! looking backward appraoch) i.e. comparing (i)-th with (i-1)-th  <<----------------
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int inc = 1;
+        int dec = 1;
+        int subarray = 1;
+        int n = nums.size();
+
+    // Using the one Step ahead pointer/iterator
+        for(int i=1; i<n ;i++){
+            if(nums[i]>nums[i-1]){
+                inc+=1;
+                dec = 1; 
+            }else if(nums[i]<nums[i-1]){
+                inc = 1;
+                dec+=1;
+            }else{
+                inc = 1;
+                dec = 1;
+            }
+            subarray = max(subarray,max(inc, dec));
+        }   
+        return subarray;
+    }
+};
