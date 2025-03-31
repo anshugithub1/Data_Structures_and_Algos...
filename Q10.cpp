863f29407d8d200c3e43969fbf6e57123b54a7f7
anshugithub1 <142107760+anshugithub1@users.noreply.github.com>
2025-03-31 19:18:18 +0530

    Update Q10.cpp "little typo"

--- Q10.cpp
+++ Q10.cpp
@@ -68,7 +68,7 @@ public:
 //------------------------------------------>>Approach-3: priority Queue - optimised (b)
 class Solution {
 public:
-    lint findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest(vector<int>& nums, int k) {
        priority_queue<int, vector<int>, greater<int>> min_heap(nums.begin(), nums.begin() + k);
         
         for (int i = k; i < nums.size(); i++) {
